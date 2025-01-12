c582a76c63d89e31baa0ac4423c0bf12a0760a7a
su11ji04 <2176400@ewhain.net>
2025-01-12 16:47:23 +0900

    HTML BASE CODE

---
+++ src/main/java/UNIS/leap_mvp/LeapMvpApplication.java
@@ -0,0 +1,13 @@
+package UNIS.leap_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UNIS/leap_mvp/LeapMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package UNIS.leap_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
